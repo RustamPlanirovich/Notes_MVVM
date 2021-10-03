b526a37a50a125e580b98c7d3ccab2d282f85f22
RustamPlanirovich <israr.ru@yandex.ru>
2021-10-04 04:17:33 +0700

    - доработал MainFragment.kt - доработал MainAdapter.kt - доработал MainFragmentViewModel.kt

--- app/src/main/java/com/nauk0a/notesmvvm/screens/main/MainFragmentViewModel.kt
+++ app/src/main/java/com/nauk0a/notesmvvm/screens/main/MainFragmentViewModel.kt
@@ -2,6 +2,9 @@ package com.nauk0a.notesmvvm.screens.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.nauk0a.notesmvvm.utilits.REPOSITORY
 
 class MainFragmentViewModel(application: Application) : AndroidViewModel(application) {
+
+    val allNotes = REPOSITORY.allNotes
 }
