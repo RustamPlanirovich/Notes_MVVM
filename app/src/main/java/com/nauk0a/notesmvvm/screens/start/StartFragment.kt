b344798ee04da2fc7d53247100b0cbcdfeb4efe0
RustamPlanirovich <israr.ru@yandex.ru>
2021-10-04 01:08:22 +0700

    - доработал StartFragmentViewModel.kt - доработал StartFragment.kt - поменял домашний экран в навигационном графе на StartFragment.kt - добавил пару констант

--- app/src/main/java/com/nauk0a/notesmvvm/screens/start/StartFragment.kt
+++ app/src/main/java/com/nauk0a/notesmvvm/screens/start/StartFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.nauk0a.notesmvvm.R
 import com.nauk0a.notesmvvm.databinding.FragmentStartBinding
+import com.nauk0a.notesmvvm.utilits.APP_ACTIVITY
 import com.nauk0a.notesmvvm.utilits.TYPE_ROOM
 
 
@@ -34,7 +35,9 @@ class StartFragment : Fragment() {
     private fun initialization() {
         mViewModel = ViewModelProvider(this).get(StartFragmentViewModel::class.java)
         mBinding.btnRoom.setOnClickListener {
-            mViewModel.initDatabase(TYPE_ROOM)
+            mViewModel.initDatabase(TYPE_ROOM){
+                APP_ACTIVITY.mNavController.navigate(R.id.action_startFragment_to_mainFragment)
+            }
         }
     }
 
